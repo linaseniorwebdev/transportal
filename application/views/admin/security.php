4d6b516e618a308af716f5d06ae5baf5ff87d00f
linaseniorwebdev <lina.web.developer.senior@gmail.com>
2019-03-09 00:08:36 -0300

    Some update

--- application/views/admin/security.php
+++ application/views/admin/security.php
@@ -10,6 +10,35 @@
 	<!-- END: Subheader -->
 
 	<div class="m-content">
-
+		<div class="m-portlet">
+			<div class="m-portlet__head">
+				<div class="m-portlet__head-caption">
+					<div class="m-portlet__head-title">
+						<h3 class="m-portlet__head-text">
+							Icon Tabs
+						</h3>
+					</div>
+				</div>
+			</div>
+			<div class="m-portlet__body">
+				<ul class="nav nav-tabs  m-tabs-line m-tabs-line--success" role="tablist">
+					<li class="nav-item m-tabs__item">
+						<a class="nav-link m-tabs__link active" data-toggle="tab" href="#logs" role="tab"><i class="la la-history"></i> Access Logs</a>
+					</li>
+					<li class="nav-item m-tabs__item">
+						<a class="nav-link m-tabs__link" data-toggle="tab" href="#settings" role="tab"><i class="la la-shield"></i> Security Setting</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="logs" role="tabpanel">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.
+					</div>
+					<div class="tab-pane" id="settings" role="tabpanel">
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+						recently.
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
